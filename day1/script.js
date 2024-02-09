fa20aee4cbcb0661b3bf02966745167ca2ca89a9
prity2502 <156299073+prity2502@users.noreply.github.com>
2024-02-09 10:06:31 +0530

    parctice javascript day 1

---
+++ day1/script.js
@@ -0,0 +1,13 @@
+// const product = {
+    title :"Pen",
+    rating :4,
+    offer : 5;
+    price : 270}; //
+
+    // cons profile = {
+        username = "Prity____02",
+        isfollow = false,
+        followers = 3426,
+        following = 123
+    }//
+    
